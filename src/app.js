1445b1ccde4b7b9211b2ca770d1785178affd6e6
gaarctic <silvagabriellabenedita@gmail.com>
2025-05-06 20:21:23 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const productsRouter = require('./routes/products.router');
+const cartsRouter = require('./routes/carts.router');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+
+module.exports = app;
